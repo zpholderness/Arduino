ad4fee203354dbcfbd54ca21cb5ca3550737c714
Zoe Holderness <zholderness3@gatech.edu>
2018-03-30 17:27:51 -0400

    Took off delay()

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   // put your setup code here, to run once:
-/LED Lights on Feedback Device
+//LED Lights on Feedback Device
 //Pin to hook LED's to, need to use an PWM Pin for Fade
 const int ledPin = 5;
 pinMode(ledPin, OUTPUT); // set as an output
@@ -16,6 +16,8 @@ int fadeFactor = 51; //constant to multiply number associated with flow type by.
  // High, 4
  // VeryHigh 5
  int endbright = 0; //The end level of brightness its transitioning too when going to a new state, eventually howbright is set equal to this
+  unsigned long currentMilli = millis();
+  unsigned long previousMilli = 0;
 }
 //either looking at flow or flowNow
 //if ActionType == Hello set to very low .5(fadeFactor)
@@ -26,19 +28,26 @@ void loop() {
 setBrightness();
 turnOnLights();
 }
-void turnOnLights() {
-  if (howBright < endbright) {
-    while(howBright<endbright) {
-      howBright= howBright+1;
-      analogWrite(ledPin, howBright); //sets the Led light to that brighness every loop. 
-      delay(100);
+
+//turns light to dimmness scale 1-5 based on flow rate. 
+//while it is increasing or decreasing to desired brightness, arduino will not be reading in data because it will be in these loops 
+// to avoid this simply take these out of the function calls and put them in the void loop(); and change while statements to if statements
+//each increase in brightness degree is 10 ms, so from off to full on (0 to 255) takes 2.55 seconds 
+void turnOnLights() { 
+
+    while(howBright < endbright)  {
+      if (previousMilli + 10) > currentMilli) {
+        howBright= howBright+1;
+        analogWrite(ledPin, howBright); //sets the Led light to that brighness every loop. 
+        previousMilli = currentMilli;
+      }
     }
-  } else {
+    while (howBright > endbright) {
+      if ((previousMilli + 100) > currentMilli) {
       howBright = howBright-1;
       analogWrite(ledPin, howBright);
-      delay(100);
     
-  }
+    }
 };
 
 void setBrightness() {
